053ad0a5640bd303be5535eaca2c26a7b0865422
Jakob Hök <jakob.hok@gmail.com>
2017-11-24 09:04:17 +0100

    Cleaned up files and improved the build_server script

--- build_server.sh
+++ build_server.sh
@@ -5,8 +5,29 @@
 # 2. Type ./build_server.sh
 # Will compile all C-files and create a runnable Server-file in bin_c
 
+FILE_NAME="Server"
+ABORT="Aborting..."
+
 cd src_c/main
 make -f Makefile
 # {rm -rfv fake_server.dSYM} &>/dev/null
 cd ../..
-mv src_c/main/simple_tcp_server Server
+mv src_c/main/server bin_c/$FILE_NAME
+
+printf "\n*****\nDo you want to transfer it to the camera? (y/n)\n"
+read  -n 1 -p ">" answer
+printf "\n"
+if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+    cd bin_c
+    read  -n 1 -p "Camera #" nbr
+    printf "\n"
+    if [ $nbr -eq $nbr 2>/dev/null ] && [ $nbr -ne 0 ]; then
+	scp $FILE_NAME rt@argus-$nbr.student.lth.se:~/
+    else
+	printf "%s\n" $ABORT 
+    fi
+else
+    printf "%s\n" $ABORT
+fi
+
+echo "$file_name"
